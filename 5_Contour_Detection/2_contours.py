ab155f920c7416cd76979d0ba1b4d05d29ac60e3
Hridoy Hasan <hridoynasah@gmail.com>
2025-05-16 19:02:36 +0600

    contours after blur the image

---
+++ 5_Contour_Detection/2_contours.py
@@ -0,0 +1,24 @@
+# contours with blur 
+
+import cv2 as cv 
+import numpy as np
+
+img = cv.imread('kashmir.jpg')
+cv.imshow('Kashmir', img)
+
+
+gray_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('gray_img', gray_img)
+
+blur_img = cv.GaussianBlur(gray_img, (5,5), cv.BORDER_DEFAULT)
+cv.imshow('blur_img', blur_img)
+
+canny_img = cv.Canny(blur_img, 125, 175)
+cv.imshow('canny_img', canny_img)
+
+contours, hierarchies = cv.findContours(canny_img, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+# print(contours, type(contours))
+# print(hierarchies, type(hierarchies))
+print(len(contours))
+
+cv.waitKey(0)
