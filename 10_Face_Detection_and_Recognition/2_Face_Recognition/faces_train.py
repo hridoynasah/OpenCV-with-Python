a1f719c9f0b3d00ce1709d0c82a031585d28204d
Hridoy Hasan <hridoynasah@gmail.com>
2025-05-18 20:27:22 +0600

    all the image added for face recognition

---
+++ 10_Face_Detection_and_Recognition/2_Face_Recognition/faces_train.py
@@ -0,0 +1,13 @@
+import os 
+import cv2 as cv
+import numpy as np
+
+people = []
+# r makes Python read the string literally, so backslashes in file paths work correctly.
+for p in os.listdir(r'D:\WorkSpace\Machine Learning\OpenCV-with-Python\10_Face_Detection_and_Recognition\2_Face_Recognition\train'):
+    people.append(p)
+
+DIR = r'D:\WorkSpace\Machine Learning\OpenCV-with-Python\10_Face_Detection_and_Recognition\2_Face_Recognition\train'
+
+features = []
+labels = []
